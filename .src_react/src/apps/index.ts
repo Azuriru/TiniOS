f84f34134dcaf1879b8a3d47de3ab2678fe19013
Azuriru <jeannelee823@gmail.com>
2024-03-30 22:36:48 +0800

    move react to its own branch

--- .src_react/src/apps/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as opera } from './opera';
-export { default as vsc } from './vsc';
-// import { default as opera } from './opera';
-// import { default as vsc } from './vsc';
-
-// export default {
-//     vsc,
-//     opera
-// };
